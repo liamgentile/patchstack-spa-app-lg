1787720cb692564c391c1cf702e440878542af67
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2022-08-29 15:14:06 -0400

    Delete index.js

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'vuex'
-
-// currentRisk state will allow us to populate EditRisk and ViewRisk page with the selected risk
-export default createStore({
-state: {
-    currentRisk: null
-  }
-})
